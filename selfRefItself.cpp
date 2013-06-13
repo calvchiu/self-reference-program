50b10a5df74988c40d7d47bb0e3d47b78101bca8
Calvin C <calchiu@ucdavis.edu>
2013-06-13 16:20:21 -0700

    second commit, added the cpp file

---
+++ selfRefItself.cpp
@@ -0,0 +1,4 @@
+#include<stdio>
+/* Let's print ourself */
+char*f="#include<stdio>%c/* Let's print ourself */%cchar*f=%c%s%c ;main(){printf(f,10,10,34,f,34,10);}%c/* Ending self print */ " ;main(){printf(f,10,10,34,f,34,10);}
+/* Ending self print */
